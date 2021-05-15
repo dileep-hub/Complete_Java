16be6d6d437f26fe40b11b37f4dec9edc7d044be
Dileep Kumar.S.R <dileepkumar1422002@gmail.com>
2021-05-15 08:52:40 +0530

    Java TypeCasting

---
+++ typeCasting/TypeCasting.java
@@ -0,0 +1,20 @@
+package typeCasting;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		
+		// auto typecasting
+//		int x = 5;
+//		double y = x;
+//		System.out.println(x);
+//		System.out.println(y);
+		
+		//manual typecasting
+	    double myDouble = 1.2222;
+	    int y = (int)myDouble;
+	    System.out.println(myDouble);
+		System.out.println(y);  // by default takes floor value
+	}
+
+}
